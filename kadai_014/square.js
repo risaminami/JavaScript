4bb6c54e98685d0672f86f83c2e5625a7c0e3973
risaminami <138634619+risaminami@users.noreply.github.com>
2023-12-05 21:26:20 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+//引数を渡す
+const squaredNumber = (num) => {
+    return num * num;
+}
+
+ console.log(squaredNumber(100));
+ console.log(squaredNumber(5));
+
+//function
+//function squaredNumber(num) {
+    //return num*num;
+//}
