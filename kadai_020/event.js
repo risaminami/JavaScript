46efc97ff90bb5540011e0ac657552110fb61782
risaminami <138634619+risaminami@users.noreply.github.com>
2023-12-15 22:05:28 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -2,12 +2,7 @@
 const btn = document.getElementById("output-btn");
 const text = document.getElementById("text");
 
-//console.log(btn.textContent);
-//btn.textContent = "クリック"
 //HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener("click", () => {
-    //const h2 = document.createElement("h2");
-    //h2.textContent="ボタンをクリックしました"
-    //document.querySelector()
     text.textContent = "ボタンをクリックしました";
 });
